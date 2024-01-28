2e179f459fe893f748f8904d0566af41bbd39cf4
Ileska <ileska@tutanota.com>
2024-01-28 17:10:15 +0200

    What??

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from fastapi.responses import RedirectResponse
 
 import services
 
-# from database import db
-# from helpers import addStatus, addUrl
+from database import db
+from helpers import addStatus, addUrl
 
 app = FastAPI()
 
@@ -20,14 +20,12 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 async def root(request: Request):
 	statuses = services.getLastStatusesWithNames()
-	print(statuses)
 	return templates.TemplateResponse("overview.html", {"request": request, "urls": statuses})
 
 @app.get("/{websiteId}")
 async def read_item(request: Request, websiteId: int):
 	name = services.getUrlFromId(websiteId)
 	stats = services.getStatsFromId(websiteId)
-	print(stats)
 	return templates.TemplateResponse("induvidual.html", {"request": request, "url": name, "stats": stats})
 
 # @app.get("/login")
@@ -37,7 +35,9 @@ async def read_item(request: Request, websiteId: int):
 
 @app.post("/refresh", response_class=RedirectResponse, status_code=303)
 async def refresh(request: Request):
+	print("Pääsi refresh alkuun")
 	services.refreshPages()
+	print("Pääsi refresh loppuun")
 	return "/"
 
 # @app.middleware("http")
